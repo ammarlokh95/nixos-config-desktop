c6c53c67b7ca34b3973ac95462d02ce17135fb61
ammarlokh95 <ammar.lokh1234@gmail.com>
2024-06-13 23:11:37 -0400

    tmux update and update flake lock

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -7,6 +7,8 @@
         tmuxPlugins.better-mouse-mode
         tmuxPlugins.vim-tmux-navigator
         tmuxPlugins.catppuccin
+        tmuxPlugins.tmux-resurrect
+        tmuxPlugins.tmux-continuum
         ];
       extraConfig = ''
           set -g default-terminal "screen-256color"
@@ -30,6 +32,16 @@
           bind -r h resize-pane -L 5
 
           bind -r m resize-pane -Z
+          
+          set-window-option -g mode-keys vi
+          
+          bind-key -T copy-mode-vi 'v' send -X begin-selection
+          bind-key -T copy-mode-vi 'y' send -X copy-selection
+
+          unbind -T copy-mode-vi MouseDragEnd1Pane
+          
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @continuum-restore 'on'
 
           set -g @catppuccin_flavour 'mocha'
         '';
